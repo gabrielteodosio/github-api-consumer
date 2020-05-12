b27952c6062934aa11e51b803fe5ef7ae7886e62
Gabriel T Marques <gabriel.marques@mesainc.com.br>
2020-05-12 19:28:55 -0300

    bundle.js added to the end of the generated bundle

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash].js',
+    filename: '[name].[hash].bundle.js',
     publicPath: '/',
   },
 };
