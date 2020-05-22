a35e8371b00d8d3d569a27b1ba55c07a3ecb49b0
Gabriel T Marques <gabriel.marques@mesainc.com.br>
2020-05-22 01:24:12 -0300

    some visual improvements and a repository filter

--- src/pages/repos/index.js
+++ src/pages/repos/index.js
@@ -1,8 +1,10 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import _ from 'lodash'
 import {connect} from 'react-redux';
 import { FixedSizeList } from 'react-window';
 
 import Grid from "@material-ui/core/Grid";
+import Input from '@material-ui/core/Input';
 import Paper from "@material-ui/core/Paper";
 import ListItem from "@material-ui/core/ListItem";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -14,6 +16,7 @@ import styles from "./repos.module.less";
 import GithubLogo from "../../images/github-logo-white.png";
 import GithubMark from "../../images/github-mark-white.png";
 import {Creators as ReposActions} from '../../redux/ducks/repos'
+import {filter} from "ramda";
 
 
 function ReposPage({
@@ -21,12 +24,41 @@ function ReposPage({
   fetchRepoCommits,
   repos: { commits, fetchingCommits, repos, fetchingRepos },
 }) {
+  const debouncedFilter = useRef(_.debounce(filterRepos, 250)).current
+  const [filterContent, setFilterContent] = useState(null)
+  const [filteredRepos, setFilteredRepos] = useState(null)
+
+  const reposData = filteredRepos || repos
+
   useEffect(() => {
     fetchRepos()
   }, [])
 
+  useEffect(() => {
+    debouncedFilter(filterContent)
+  }, [filterContent])
+
+
+  function handleFilterChange(e) {
+    setFilterContent(e.target.value)
+  }
+
+  function filterRepos(text) {
+    let filtered = []
+
+    if (text) {
+      filtered = repos.filter(({ full_name: fullName }) => fullName.toLowerCase().includes(text.toLowerCase()))
+    }
+
+    if (filtered.length > 0 || text.length > 0) {
+      setFilteredRepos(filtered.slice(0, 1));
+    } else {
+      setFilteredRepos(null);
+    }
+  }
+
   function handleRenderRepository({ index, style }) {
-    const repo = repos[index]
+    const repo = reposData[index]
     const { full_name: fullName, name } = repo
 
     const handleClick = () => {
@@ -69,30 +101,35 @@ function ReposPage({
     <Grid container component="main" className={styles.root}>
       <CssBaseline/>
       <Grid item xs={12} sm={8} md={5} square className={styles.paper}>
-        <img src={GithubLogo} className={styles.avatar}/>
+        <img src={GithubLogo} className={styles.avatar} alt={'github-logo'}/>
       </Grid>
       <Grid item xs={12} sm={4} md={7} component={Paper} elevation={6} className={styles.contentContainer}>
         <Grid container item component='div' className={styles.lists}>
           <Grid item xs={12} sm={12} md={6} className={styles.list}>
             <h2>Repositórios</h2>
+            <Input
+              autoFocus
+              onChange={handleFilterChange}
+              style={{ margin: '0 20px 10px 20px' }}
+            />
             {fetchingRepos ? (
               <LinearProgress color={'primary'} classes={{ barColorPrimary: styles.loading }} />
             ) : (
               Array.isArray(repos) ? (
-                <FixedSizeList height={500} width={'100%'} itemSize={46} itemCount={repos.length}>
+                <FixedSizeList height={470} width={'100%'} itemSize={46} itemCount={reposData.length}>
                   {handleRenderRepository}
                 </FixedSizeList>
               ) : null
             )}
           </Grid>
 
-          <Grid item xs={12} sm={12} md={6} className={styles.list}>
+          <Grid item xs={12} sm={12} md={6} className={`${styles.list} ${styles.noSearch}`}>
             <h2>Commits</h2>
             {fetchingCommits ? (
               <LinearProgress color={'primary'} classes={{ barColorPrimary: styles.loading }} />
             ) : (
               Array.isArray(commits) ? (
-                <FixedSizeList height={500} width={'100%'} itemSize={100} itemCount={commits.length}>
+                <FixedSizeList height={470} width={'100%'} itemSize={130} itemCount={commits.length}>
                   {handleRenderCommit}
                 </FixedSizeList>
               ) : null
